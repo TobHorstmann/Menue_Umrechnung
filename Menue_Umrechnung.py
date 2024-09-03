433038eff57a1d08fae75b622e9d03a6356a935a
TobHorstmann <tabbelnu1@gmail.com>
2024-09-03 09:53:32 +0200

    Update Menue_Umrechnung.py
    Version 03.09

--- Menue_Umrechnung.py
+++ Menue_Umrechnung.py
@@ -1,7 +1,8 @@
 #Menü-Aufbau mit While-Schleife
 while True:
     print ("Was möchten Sie umrechnen?")
-    print ("""(1) Zentimeter in Zoll
+    
+    print ("""\n(1) Zentimeter in Zoll
 (2) Kilometer in Meilen
 (3) Quadratkilometer in Hektar
 (4) Kilometer/Stunde in Meter/Sekunde
@@ -11,7 +12,7 @@ while True:
  
 #Auswahl der Umrechnungsmöglichkeiten
     
-    auswahl = input("Ihre Auswahl ")
+    auswahl = input("\nIhre Auswahl ")
     
 #Umrechnung Zentimeter in Zoll    
     if auswahl == "1":
@@ -19,40 +20,48 @@ while True:
         zenti = float(input("Eingabe Zentimeter: "))
         zoll = zenti * 0.3937
         print (zenti, "cm sind " ,zoll, "Zoll")
+        
 #Umrechnung Kilometer in Meilen        
     elif auswahl == "2":
         print ("(2) Kilometer in Meilen")
         kilom = float(input("Eingabe Kilometer: "))
         meilen = kilom * 0.6213
         print (kilom, "Kilometer sind " ,meilen , "Meilen")
+        
 #Umrechnung Quadratkilometer in Hektar         
     elif auswahl == "3":
         print ("(3) Quadratkilometer in Hektar")
         quad = float(input("Eingabe Quadratkilometer: "))
         ha = quad * 100
         print (quad, "Quadratkilometer sind ", ha, "Hektar")
-#Umrechnung Kilometer/Stunde in Meter/Sekunde    
+        
+#Umrechnung Kilometer/Stunde in Meter/Sekunde        
     elif auswahl == "4":
         print ("(4) Kilometer/Stunde in Meter/Sekunde")
         kmh = float(input("Eingabe Kilometer/Stunde: "))
         ms = kmh / 3.6
         print (kmh, "Kilometer/Stunde sind " , ms, "Meter/Sekunde")
+        
 #Umrechnung Kilobyte in Gigabyte
     elif auswahl == "5":
         print ("(5) Kilobyte in Gigabyte")
         kilob = float(input("Eingabe Kilobyte: "))
         gigab = kilob / 1000000
         print (kilob, "Kilobyte sind" , gigab, "Gigabyte")
+        
 #Auswahl Menü beenden
     elif auswahl == "0":
         break
+    
 #Falsche Auswahl
     else:
-        print ("Ungültige Auswahl")
-    
-                    
+        print ("Ungültige Auswahl.Bitte wählen Sie zwischen 1-5 oder 0 zum Beenden!")
         
-        
+#Abfrage weiterer Umrechnungen        
+    weiter = input ("\nMöchtest du eine weitere Umrechnung durchführen? (ja/nein): ").strip().lower()
+    if weiter != 'ja':
+        print("Programm wird beendet.")
+        break        
             
         
         
